54dc26ba45d47c79eb597c2fbad698ce8ac9618c
naitik3896 <n7853175@gmail.com>
2026-02-07 21:13:47 +0530

    Add a simple HTTP GET handler for API

---
+++ api/hello.py
@@ -0,0 +1,13 @@
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+
+        self.wfile.write(json.dumps({
+            "status": "success",
+            "message": "API working from GitHub + Vercel"
+        }).encode())
